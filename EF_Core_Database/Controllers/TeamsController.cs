c1aa752259e96585c72c037b48668824ff5a8734
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-08-19 22:31:34 +0800

    Added TeamsController

---
+++ EF_Core_Database/Controllers/TeamsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EF_Core_Database.Data;
+
+namespace EF_Core_Database.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var teams = await _context.Teams
+                .Include(t => t.Department)
+                .Include(t => t.TeamMembers)
+                .OrderBy(t => t.Name)
+                .ToListAsync();
+
+            return View(teams);
+        }
+    }
+}
