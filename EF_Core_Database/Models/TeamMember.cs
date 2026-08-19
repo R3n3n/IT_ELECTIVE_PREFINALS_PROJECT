fce5ac725691200abe46ca48c1fadb8034cda2a5
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-08-19 22:24:09 +0800

    Added TeamMember

---
+++ EF_Core_Database/Models/TeamMember.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Core_Database.Models
+{
+    [Table("TeamMembers")]
+    [PrimaryKey(nameof(TeamId), nameof(EmployeeId))]
+    public class TeamMember
+    {
+        [Required]
+        public int TeamId { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string JoinedAt { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(TeamId))]
+        public Team Team { get; set; } = null!;
+
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; } = null!;
+    }
+}
