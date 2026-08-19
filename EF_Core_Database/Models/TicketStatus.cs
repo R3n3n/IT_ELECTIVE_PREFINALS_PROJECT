d14c6b7743e6fa0127a661f2257fd59a10fd3d26
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-08-19 22:33:41 +0800

    Added TicketStatus

---
+++ EF_Core_Database/Models/TicketStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Core_Database.Models
+{
+    [Table("TicketStatuses")]
+    public class TicketStatus
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public bool IsClosed { get; set; }
+    }
+}
