cb86774a7913d11ab7b3f65997e9f9421292c22d
lloydnathanielarciaga <lloydnathanielarciaga@gmail.com>
2026-08-19 22:42:05 +0800

    Added Customer, Team, TeamMember, TicketPrioritie,  TicketStatuse, dbset

--- EF_Core_Database/Data/ApplicationDbContext.cs
+++ EF_Core_Database/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EF_Core_Database.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EF_Core_Database.Data
 {
@@ -6,7 +7,11 @@ namespace EF_Core_Database.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-
         }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<TicketPriority> TicketPriorities { get; set; }
+        public DbSet<TicketStatus> TicketStatuses { get; set; }
     }
 }
