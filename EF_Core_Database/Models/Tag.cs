e5967880193eaca098093fd62bd06e17a760ae3d
Renen <Renen@DESKTOP-1B94TU9>
2026-08-19 22:09:36 +0800

    Added Tag Model

---
+++ EF_Core_Database/Models/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Core_Database.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<TicketTag> TicketTags { get; set; } = new List<TicketTag>();
+
+    }
+}
