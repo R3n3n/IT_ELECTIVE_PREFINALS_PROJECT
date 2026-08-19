aa61f240162ddf1f910f29261fd9ff8d136fb827
Renen <Renen@DESKTOP-1B94TU9>
2026-08-19 22:03:39 +0800

    Added Department Model

---
+++ EF_Core_Database/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EF_Core_Database.Models
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
