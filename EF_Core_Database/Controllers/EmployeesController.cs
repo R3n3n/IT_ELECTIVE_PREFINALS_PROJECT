ec055fbddc9db30b4ba90e0e5fc53bbc8ca069b5
Renen <Renen@DESKTOP-1B94TU9>
2026-08-19 22:22:10 +0800

    Added Employees Controller

---
+++ EF_Core_Database/Controllers/EmployeesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EF_Core_Database.Data;
+
+namespace EF_Core_Database.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var employees = await _context.Employees
+                .Include(e => e.Department)
+                .OrderBy(e => e.LastName)
+                .ThenBy(e => e.FirstName)
+                .ToListAsync();
+
+            return View(employees);
+        }
+    }
+}
