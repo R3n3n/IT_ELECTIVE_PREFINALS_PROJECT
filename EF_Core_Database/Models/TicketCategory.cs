65711fa22d598a17daee44f78cecca14cc87d618
Eirene Karunungan <eireneysabelkarunungan@gmail.com>
2026-08-20 15:21:18 +0800

    Added TicketCategory model

---
+++ EF_Core_Database/Models/TicketCategory.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
+
+namespace EF_Core_Database.Models
+{
+    [Table("TicketCategories")]
+    public class TicketCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int? ParentCategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [ForeignKey(nameof(ParentCategoryId))]
+        public TicketCategory? ParentCategory { get; set; }
+
+        public ICollection<TicketCategory> ChildCategories { get; set; } = new List<TicketCategory>();
+
+        public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+    }
+}
