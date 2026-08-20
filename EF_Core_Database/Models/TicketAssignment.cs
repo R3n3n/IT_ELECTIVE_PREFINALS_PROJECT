2090a0ab15d8da7d6350e2f39e5a2ee17f7b9697
Eirene Karunungan <eireneysabelkarunungan@gmail.com>
2026-08-20 15:12:14 +0800

    Added TicketAssignment model

---
+++ EF_Core_Database/Models/TicketAssignment.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
+
+namespace EF_Core_Database.Models
+{
+    [Table("TicketAssignments")]
+    [PrimaryKey(nameof(TicketId), nameof(EmployeeId))]
+    public class TicketAssignment
+    {
+        [Required]
+        public int TicketId { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public DateTime AssignedAt { get; set; }
+
+        public DateTime? UnassignedAt { get; set; }
+
+        public bool IsPrimary { get; set; }
+
+        [ForeignKey(nameof(TicketId))]
+        public Ticket Ticket { get; set; } = null!;
+
+        [ForeignKey(nameof(EmployeeId))]
+        public Employee Employee { get; set; } = null!;
+    }
+}
